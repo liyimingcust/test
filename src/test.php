373c6ed14378b1e0f769de4ed35f9cdd5a438f52
李一鸣 <921598184@qq.com>
2019-03-18 10:27:10 +0800

    another test

--- src/test.php
+++ src/test.php
@@ -4,7 +4,7 @@ class Test{
 	public function test(){
 		echo __FILE__;
 		echo "\n";
-		echo  "this should be displayed only when tag is pushed";
+		echo  "this  11 should be displayed only when tag is pushed";
 	}
 
 }
